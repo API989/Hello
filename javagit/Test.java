e142fe1cacff4eb687103424731adcb0ceedaec6
Trainer <radha.shraogi@apisero.com>
2021-02-12 17:52:41 +0530

    It is for java

---
+++ javagit/Test.java
@@ -0,0 +1,40 @@
+package Assign;
+
+public class Test {
+	public static void main(String[] args) {
+		Test t=new Test();
+		int arr[]=new int[2];
+		int a=2;
+		int b=4;
+		int c;
+		arr[0]=1;
+		arr[1]=2;
+		System.out.println(arr);
+		try
+		{
+			c=a/b;
+			arr[5]=6;
+			System.out.println("Hello");
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Exception is handled"+ e);
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("Exception is handled"+ e);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception is handled"+ e);
+		}
+		finally
+		{
+			System.out.println("Finally is executed");
+		}
+		System.out.println("After finally");
+
+	}
+
+}
+
